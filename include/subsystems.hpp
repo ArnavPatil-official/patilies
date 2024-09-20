cb63b885fd47e904d239adecfa37310c70985759
Arnav <arnavrpatil0@gmail.com>
2024-09-20 17:26:10 -0400

    first commit

---
+++ include/subsystems.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "api.h"
+#include "main.h"
+using namespace pros;
+
+
+// Your motors, sensors, etc. should go here.  Below are examples
+
+inline pros::Motor intake(1);
+inline pros::Motor arm(2);
+// inline pros::adi::DigitalIn limit_switch('A');
